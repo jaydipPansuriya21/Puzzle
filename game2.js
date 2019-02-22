cf096cbe0339ae6b47552a83d831f6971ac6e598
Jaydip <pansuriyajaydip21@gmail.com>
2019-02-22 17:48:29 +0530

    all done

--- game2.js
+++ game2.js
@@ -1,25 +1,37 @@
 
-function start(){
-  for(var i =1;i<10;i++)
-  document.getElementById(i).disabled  = true;
-
-}
-start();
-
-
-
-
-
-
-
-
-
 var pair = [123 , 147 , 159 , 258 ,456 , 357 , 369 , 789];
 var userX = [];
 var userY = [];
 var state = true;
 
 var ComputerSelector ;
+
+
+function reset(){
+  //console.log(userX);
+  userX = [];
+  userY = [];
+
+  //console.log(userY);
+ enable();
+document.getElementById('result').innerHTML = "";
+document.getElementById('X').style.backgroundColor = "rgba(255,0,0,0.7)";
+document.getElementById('Y').style.backgroundColor = " white";
+document.getElementById('body').style.backgroundColor = " white";
+state = true;
+  }
+
+function enable(){
+   for(var i =1;i<10;i++){
+  document.getElementById(i).disabled = false;
+  document.getElementById(i).innerHTML = i;
+}
+}
+function disable(){
+   for(var i =1;i<10;i++){
+  document.getElementById(i).disabled = true;
+}
+}
 
 
 
@@ -29,6 +41,8 @@ document.getElementById(value).innerHTML= " X ";
 document.getElementById(value).disabled = true;
 getItem("x", value);
 state = false;
+document.getElementById('X').style.backgroundColor = " white";
+document.getElementById('Y').style.backgroundColor = " rgba(255,0,0,0.7)";
 }
 else
 {
@@ -36,6 +50,9 @@ document.getElementById(value).innerHTML= " O ";
 document.getElementById(value).disabled = true;
 getItem("o", value);
 state = true;
+document.getElementById('Y').style.backgroundColor = " white";
+document.getElementById('X').style.backgroundColor = "rgba(255,0,0,0.7)";
+
 }
 
 }
@@ -47,7 +64,14 @@ userX.push(value);
 userX.sort();
 if(checkForResult(userX))
 {
+document.getElementById('Y').style.backgroundColor = " rgba(255,0,0,0.7)";
+document.getElementById('X').style.backgroundColor = "rgba(255,0,0,0.7)";
+
   console.log("user X win..  ");
+  disable();
+  document.getElementById('result').innerHTML = "....  &nbsp;&nbsp;&nbsp;&nbsp; PLAYER   &nbsp;&nbsp;&nbsp;&nbsp;X &nbsp;&nbsp;&nbsp;&nbsp;  WIN &nbsp;&nbsp;&nbsp;&nbsp;  ....";
+document.getElementById('body').style.backgroundColor = "black";
+
 }
 }
 else {
@@ -55,7 +79,13 @@ userY.push(value);
 userY.sort();
 if(checkForResult(userY))
 {
+
+document.getElementById('Y').style.backgroundColor = " rgba(255,0,0,0.7)";
+document.getElementById('X').style.backgroundColor = " rgba(255,0,0,0.7)";
   console.log("user Y win..  ");
+  disable();
+document.getElementById('result').innerHTML = "....  &nbsp;&nbsp;&nbsp;&nbsp; PLAYER   &nbsp;&nbsp;&nbsp;&nbsp;O &nbsp;&nbsp;&nbsp;&nbsp;  WIN &nbsp;&nbsp;&nbsp;&nbsp;  ....";
+document.getElementById('body').style.backgroundColor = " black";
 }
 }
 console.log(userX);
